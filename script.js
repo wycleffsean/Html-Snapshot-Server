e2da2b9ac9ed4047597a84911edfd27c8c4cddd0
unknown <scarey@SCAREY.artisinc.local>
2013-11-18 15:30:18 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿var page = require('webpage').create(),
+	system = require('system');
+
+var url = system.args[1];
+page.open(url, function(status) {
+	page.evaluate(function() {});	
+	console.log(page.content);
+	phantom.exit();
+});
---
+++ server.rb
@@ -0,0 +1,28 @@
+﻿require 'webrick'
+include WEBrick
+
+class Simple < HTTPServlet::AbstractServlet
+	def do_GET request, response
+		uri = request.unparsed_uri
+		#p uri
+		response['Content-Type'] = request.content_type
+		response.body = `phantomjs script.js "http://localhost:3000#{uri}"`
+		raise HTTPStatus::OK
+	end
+
+	alias_method :do_POST, :do_GET
+end
+
+def start_server(config = {})
+	config.update(Port:2000)
+	server = HTTPServer.new(config)
+	yield server if block_given?
+	['INT', 'TERM'].each {|signal| trap(signal) { server.shutdown }} # handle termination
+	server.start
+end
+
+start_server do |server|
+	server.mount '/', Simple
+end
+
+
